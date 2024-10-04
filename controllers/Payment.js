9330eaa9a53c3650d45fa44ca6834ca739cf4c2a
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-10-04 23:12:20 +0530

    Write logic for Verify the Signature of the Razorpay Payment gateway

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -88,3 +88,79 @@ exports.capturePayment = async (req, res) => {
 
 };
 
+//verify Signature of Razorpay and Server
+
+exports.verifySignature = async (req, res) => {
+    const webhookSecret = "12345678";
+
+    const signature = req.headers["x-razorpay-signature"];
+
+    const shasum =  crypto.createHmac("sha256", webhookSecret);
+    shasum.update(JSON.stringify(req.body));
+    const digest = shasum.digest("hex");
+
+    if(signature === digest) {
+        console.log("Payment is Authorised");
+
+        const {courseId, userId} = req.body.payload.payment.entity.notes;
+
+        try{
+                //fulfil the action
+
+                //find the course and enroll the student in it
+                const enrolledCourse = await Course.findOneAndUpdate(
+                                                {_id: courseId},
+                                                {$push:{studentsEnrolled: userId}},
+                                                {new:true},
+                );
+
+                if(!enrolledCourse) {
+                    return res.status(500).json({
+                        success:false,
+                        message:'Course not Found',
+                    });
+                }
+
+                console.log(enrolledCourse);
+
+                //find the student andadd the course to their list enrolled courses me 
+                const enrolledStudent = await User.findOneAndUpdate(
+                                                {_id:userId},
+                                                {$push:{courses:courseId}},
+                                                {new:true},
+                );
+
+                console.log(enrolledStudent);
+
+                //mail send krdo confirmation wala 
+                const emailResponse = await mailSender(
+                                        enrolledStudent.email,
+                                        "Congratulations from CodeHelp",
+                                        "Congratulations, you are onboarded into new CodeHelp Course",
+                );
+
+                console.log(emailResponse);
+                return res.status(200).json({
+                    success:true,
+                    message:"Signature Verified and COurse Added",
+                });
+
+
+        }       
+        catch(error) {
+            console.log(error);
+            return res.status(500).json({
+                success:false,
+                message:error.message,
+            });
+        }
+    }
+    else {
+        return res.status(400).json({
+            success:false,
+            message:'Invalid request',
+        });
+    }
+
+
+};
