b5a8c351e78ee13cc6c3bc6839b55462d7806500
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-10-04 23:23:31 +0530

    Wrie Image Upoloader

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -11,7 +11,7 @@ const mailSender = async (email, title, body) => {
     });
 
     let info = await transporter.sendMail({
-      from: "StudyNotion || CodeHelp - By Biswa",
+      from: "StudyNotion || By Biswaranjan Mohanty",
       to: `${email}`,
       subject: `${title}`,
       html: `${body}`,
