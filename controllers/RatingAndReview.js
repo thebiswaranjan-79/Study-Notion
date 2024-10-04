1f42af435b600330db8a8b41a990690d55d81450
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-10-04 23:31:31 +0530

    Added Create Rating and Reviews Controller

---
+++ controllers/RatingAndReview.js
@@ -0,0 +1,67 @@
+const RatingAndReview = require("../models/RatingAndRaview");
+const Course = require("../models/Course");
+const { mongo, default: mongoose } = require("mongoose");
+
+//createRating
+exports.createRating = async (req, res) => {
+    try{
+
+        //get user id
+        const userId = req.user.id;
+        //fetchdata from req body
+        const {rating, review, courseId} = req.body;
+        //check if user is enrolled or not
+        const courseDetails = await Course.findOne(
+                                    {_id:courseId,
+                                    studentsEnrolled: {$elemMatch: {$eq: userId} },
+                                });
+
+        if(!courseDetails) {
+            return res.status(404).json({
+                success:false,
+                message:'Student is not enrolled in the course',
+            });
+        }
+        //check if user already reviewed the course
+        const alreadyReviewed = await RatingAndReview.findOne({
+                                                user:userId,
+                                                course:courseId,
+                                            });
+        if(alreadyReviewed) {
+                    return res.status(403).json({
+                        success:false,
+                        message:'Course is already reviewed by the user',
+                    });
+                }
+        //create rating and review
+        const ratingReview = await RatingAndReview.create({
+                                        rating, review, 
+                                        course:courseId,
+                                        user:userId,
+                                    });
+       
+        //update course with this rating/review
+        const updatedCourseDetails = await Course.findByIdAndUpdate({_id:courseId},
+                                    {
+                                        $push: {
+                                            ratingAndReviews: ratingReview._id,
+                                        }
+                                    },
+                                    {new: true});
+        console.log(updatedCourseDetails);
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"Rating and Review created Successfully",
+            ratingReview,
+        })
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        })
+    }
+}
+
